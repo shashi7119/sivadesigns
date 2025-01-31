3009cc6ae9fb7e6b12a23fc07a625cd3c2fce7c5
Sasikumar <sasikumar.periyasamy@gmail.com>
2025-02-01 00:27:15 +0530

    Customer Master Integration #1

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -13,11 +13,15 @@ DataTable.use(DT);
 function Customer() {
 
     const [formData, setFormData] = useState({
-        mname: '',type:'customer'
+        mname: '',type:'customer',email:'',contact_number:''
+        ,address1:'',address2:'',pincode:'',gstin:''
       });
 
       const regexPatterns = {
-        mname: /^[a-zA-Z0-9_@./#&+\-, ]*$/,          // Only numbers for input1
+        mname: /^[a-zA-Z0-9_@./#&+\-, ]*$/,email: /^[a-zA-Z0-9_@./#&+\-, ]*$/,
+        address1: /^[a-zA-Z0-9_@./#&+\-, ]*$/,contact_number: /^[0-9 ]*$/,
+        address2: /^[a-zA-Z0-9_@./#&+\-, ]*$/,pincode: /^[0-9 ]*$/,
+        gstin: /^[a-zA-Z0-9]*$/
       };
 
     const [tableData, setTableData] = useState([ ]);
@@ -108,6 +112,7 @@ function Customer() {
             </div>
        </Row>
     <DataTable data={tableData} options={{
+      order: [[0, 'desc']],
                 responsive: true,
                 select: true,
                 iDisplayLength:25,
@@ -129,14 +134,101 @@ function Customer() {
         <Modal.Header closeButton>
           <Modal.Title>Add Customer</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body style={{
+      maxHeight: '400px',
+      overflowY: 'auto'
+     }}>
           <Form>
-            <Form.Group className="mb-3" >
+            <Form.Group className="mb-2" >
               <Form.Label>Customer Name</Form.Label>
               <Form.Control
               type="text"
               name="mname"              
               value={formData.mname}
+              onKeyUp={handleKeyUp}
+              onChange={(e) =>  setFormData((prevData) => ({
+                ...prevData,
+                [e.target.name]: e.target.value // Update the value of the specific input field
+              }))}    
+             required
+            />
+            </Form.Group>
+            <Form.Group className="mb-2" >
+              <Form.Label>Contact Number</Form.Label>
+              <Form.Control
+              type="text"
+              name="contact_number"              
+              value={formData.contact_number}
+              onKeyUp={handleKeyUp}
+              onChange={(e) =>  setFormData((prevData) => ({
+                ...prevData,
+                [e.target.name]: e.target.value // Update the value of the specific input field
+              }))}    
+             required
+            />
+            </Form.Group>
+            <Form.Group className="mb-2" >
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+              type="text"
+              name="email"              
+              value={formData.email}
+              onKeyUp={handleKeyUp}
+              onChange={(e) =>  setFormData((prevData) => ({
+                ...prevData,
+                [e.target.name]: e.target.value // Update the value of the specific input field
+              }))}    
+             
+            />
+            </Form.Group>
+            <Form.Group className="mb-2" >
+              <Form.Label>Address 1</Form.Label>
+              <Form.Control
+              type="text"
+              name="address1"              
+              value={formData.address1}
+              onKeyUp={handleKeyUp}
+              onChange={(e) =>  setFormData((prevData) => ({
+                ...prevData,
+                [e.target.name]: e.target.value // Update the value of the specific input field
+              }))}    
+             required
+            />
+            </Form.Group>
+            <Form.Group className="mb-2" >
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control
+              type="text"
+              name="address2"              
+              value={formData.address2}
+              onKeyUp={handleKeyUp}
+              onChange={(e) =>  setFormData((prevData) => ({
+                ...prevData,
+                [e.target.name]: e.target.value // Update the value of the specific input field
+              }))}    
+             
+            />
+            </Form.Group>
+            <Form.Group className="mb-2" >
+              <Form.Label>Pincode</Form.Label>
+              <Form.Control
+              type="text"
+              name="pincode"              
+              value={formData.pincode}
+              onKeyUp={handleKeyUp}
+              onChange={(e) =>  setFormData((prevData) => ({
+                ...prevData,
+                [e.target.name]: e.target.value // Update the value of the specific input field
+              }))}    
+             required
+            />
+            </Form.Group>
+            <Form.Group className="mb-2" >
+              <Form.Label>GSTIN</Form.Label>
+              <Form.Control
+              type="text"
+              name="gstin"              
+              value={formData.gstin}
               onKeyUp={handleKeyUp}
               onChange={(e) =>  setFormData((prevData) => ({
                 ...prevData,
